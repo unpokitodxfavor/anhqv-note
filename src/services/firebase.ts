1ab80ce0e02f9a2a6e3eec1fc5c242b4b65df989
Unpokitodxfavor <unpokitodxfavor@aquinohayquienviva.es>
2026-02-13 14:19:09 +0100

    chore: configurar credenciales reales de Firebase e inicializar servicios

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace with your Firebase project configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAos57Ls9a2F5Dcl9ALelcrEMV4TbXgwLo",
+    authDomain: "anhqv-note-clemente-1337.firebaseapp.com",
+    projectId: "anhqv-note-clemente-1337",
+    storageBucket: "anhqv-note-clemente-1337.firebasestorage.app",
+    messagingSenderId: "857498246633",
+    appId: "1:857498246633:web:cf1c182f0d63a0e459bf78"
 };
 
 const app = initializeApp(firebaseConfig);
